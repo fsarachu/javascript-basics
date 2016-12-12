38b919938bdfd3d72215b056e2b3ae3126493cd9
fsarachu <fsarachu@gmail.com>
2016-12-12 12:52:12 -0300

    string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,7 @@
-$("#main").append("Franco Sarachu");
-
 var awesomeThoughts = "I am Franco and I'm AWESOME!";
 
 console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
