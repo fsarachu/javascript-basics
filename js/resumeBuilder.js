5b1d85900cb7e51210bc50ea8275740b7ead837b
fsarachu <fsarachu@gmail.com>
2016-12-13 13:36:24 -0300

    Add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,23 @@
-var name = "Franco Sarachu";
-var role = "Fullstack Web Developer";
+var bio = {
+    "name": "Franco Sarachu",
+    "role": "Fullstack Web Developer",
+    "contacts": {
+        "email": "fsarachu@gmail.com",
+        "github": "fsarachu",
+        "mobile": "+59899123456",
+        "location": "Maldonado, Uruguay"
+    },
+    "welcomeMessage": "Hey! I'm Franco and I can write beautiful code for you!",
+    "skills": [
+        "Javascript", "Css3/Sass", "Html5", "Python", "Php"
+    ],
+    "bioPic": "images/fry.jpg"
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-var $header = $("#header");
-
-$header.prepend(formattedRole);
-$header.prepend(formattedName);
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//
+// var $header = $("#header");
+//
+// $header.prepend(formattedRole);
+// $header.prepend(formattedName);
